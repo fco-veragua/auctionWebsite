cbced7b3d3e6e0da3480428e5708bbf123e63ff8
fco-veragua <franciscoveraguacabrera@gmail.com>
2022-05-16 19:01:00 +0200

    Added method to Get all auctions

--- src/Controller/AuctionController.php
+++ src/Controller/AuctionController.php
@@ -2,17 +2,30 @@
 
 namespace App\Controller;
 
+use App\Repository\AuctionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AuctionController extends AbstractController
 {
+    private $auctionRepository;
+
+    public function __construct(AuctionRepository $auctionRepository)
+    {
+        $this->auctionRepository = $auctionRepository;
+    }
+
     #[Route('/auction', name: 'app_auction')]
     public function index(): Response
     {
+        $auctions = $this->auctionRepository->findAll();
+        // dd($auctions); // Filter the inner elements of doctrine
+
         return $this->render('auction/index.html.twig', [
-            'controller_name' => 'AuctionController',
+            'auctions' => $auctions
         ]);
+
+        // missing add error if no auctions found (404...)
     }
 }
