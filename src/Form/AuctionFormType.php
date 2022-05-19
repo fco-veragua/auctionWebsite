abb6e31d9ec0d55bd2248eec9ae79d5b98b54521
fco-veragua <franciscoveraguacabrera@gmail.com>
2022-05-19 19:18:43 +0200

    Modify Form to create art auctions

--- src/Form/AuctionFormType.php
+++ src/Form/AuctionFormType.php
@@ -7,18 +7,47 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Vich\UploaderBundle\Form\Type\VichImageType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 
 class AuctionFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('description')
-            ->add('state')
-            ->add('price')
-            ->add('startDate')
-            ->add('closingDate')
+            ->add('title', TextType::class, [
+                'attr' => array(
+                    'class' => '',
+                    'placeholder' => 'Enter title...'
+                )
+            ])
+            ->add('description', TextareaType::class, [
+                'attr' => array(
+                    'class' => '',
+                    'placeholder' => 'Enter description...'
+                )
+            ])
+            ->add('state', TextType::class, [
+                'attr' => array(
+                    'class' => '',
+                    'placeholder' => 'Enter state...'
+                )
+            ])
+            ->add('price', IntegerType::class, [
+                'attr' => array(
+                    'class' => '',
+                    'placeholder' => 'Enter price...'
+                )
+            ])
+            //->add('startDate')
+            ->add('closingDate', DateTimeType::class, [
+                'attr' => array(
+                    'class' => '',
+                    'placeholder' => ''
+                )
+            ])
             ->add('photosFile', VichImageType::class, [
                 'required' => false,
                 'allow_delete' => true,
@@ -29,9 +58,9 @@ class AuctionFormType extends AbstractType
                 'imagine_pattern' => null,
                 'asset_helper' => true,
             ])
-            ->add('updateAt')
+            //->add('updateAt')
             //->add('category')
-            // ->add('user')
+            //->add('user')
         ;
     }
 
