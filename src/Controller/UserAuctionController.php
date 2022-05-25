357dbff2397eb496b361624b6fa93f18c2096f58
fco-veragua <franciscoveraguacabrera@gmail.com>
2022-05-25 18:33:12 +0200

    Created UserAuctionController

---
+++ src/Controller/UserAuctionController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserAuctionController extends AbstractController
+{
+    #[Route('/user/auction', name: 'app_user_auction')]
+    public function index(): Response
+    {
+        return $this->render('user_auction/index.html.twig', [
+            'controller_name' => 'UserAuctionController',
+        ]);
+    }
+}
