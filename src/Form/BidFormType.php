3bb5ff4af787191fdf862e6cb96f83b287f2f5bf
fco-veragua <franciscoveraguacabrera@gmail.com>
2022-05-26 19:00:16 +0200

    Generate Bid form

---
+++ src/Form/BidFormType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\UserAuction;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class BidFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('bidDate')
+            ->add('bidValue')
+            ->add('auction')
+            ->add('user')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => UserAuction::class,
+        ]);
+    }
+}
